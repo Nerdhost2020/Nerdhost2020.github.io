d8bf905885c17aa99f07bae5cbb8de4ba2a580ae
Nerdhost2020 <matt.steckbauer+6@gmail.com>
2020-04-01 15:17:45 -0400

    Js

--- all.js
+++ all.js
@@ -1,14 +1,7 @@
 //$("blockquote").append("<span class='imageholder'>");
-//$(".hcb-comment-tb").prepend("</span>");
-function checkforimage() {
-  
-};
+//$(".hcb-comment-tb").prepend("</span>")
 $(document).ready(function() {
-  
-  $('a[href*="/storage/"]').parent.each(function() {
-    $( this ).css({"background-color": "red", "border": "10px solid red"});
-    alert("Hello! I am an alert box!!");
- });
- 
- //console.log("ready");
+
+  $("a[href*='/storage/lg']:parent").addClass('test');
+
 });
