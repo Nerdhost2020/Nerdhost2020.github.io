8f590c685e844bc5bef60f7a044f7e4a450a30de
Nerdhost2020 <matt.steckbauer+6@gmail.com>
2020-04-01 15:35:27 -0400

    Js

--- all.js
+++ all.js
@@ -1,5 +1,3 @@
-//$("blockquote").append("<span class='imageholder'>");
-//$(".hcb-comment-tb").prepend("</span>")
 $(document).ready(function() {
 
   $("a[href*='/storage/lg']:parent").addClass('test');
